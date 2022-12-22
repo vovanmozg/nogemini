b1d108225ff81882901b1545cba7b4daebd74c77
Vladimir Polukhin <vovanmozg@gmail.com>
2022-12-22 23:21:18 +0300

    Add ability to change log out and level

--- src/config/logger.rb
+++ src/config/logger.rb
@@ -1,7 +1,15 @@
 require 'logger'
-LOGGER = Logger.new('logfile.log')
-LOGGER.level = Logger::ERROR
+# TODO: Use arguments to pass LOG_OUT and LOG_LEVEL
+
+LOGGER = Logger.new(
+  ENV['LOG_OUT'] == 'STDOUT' ? STDOUT : 'logfile.log',
+  level: ENV['LOG_LEVEL'] || 'ERROR'
+)
 
 def debug(msg)
   LOGGER.debug(msg)
+end
+
+def warning(msg)
+  LOGGER.warn(msg)
 end
--- src/index.rb
+++ src/index.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 # Calculates phash for input files and cache result to _info.txt in the
 # directory where the files placed.
 # Using example
-# ruby calculate-phash.rb /path/to/images
+#  ruby index.rb /path/to/images
 
 require './src/config/initialize'
 require './src/lib/image_iterator'
@@ -10,7 +12,8 @@ require './src/lib/readers/chain'
 require './src/lib/readers/image_props'
 require './src/lib/readers/phash'
 
-# TODO: add argument to clear index
+# TODO: Add argument to clear index
+# TODO: Add argument for logger engine and level
 path = ARGV[0] || raise('Invalid directory')
 
 # TODO: Add program argument for reader strategy
